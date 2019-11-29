b363b4332a01fd56bbcf1ba243ce7c4c4a1b4a91
rahul bhalekar <rahul.b@shivaami.com>
2019-11-29 10:49:05 +0530

    script to take back up of the file

---
+++ file_auto_backup.js
@@ -0,0 +1,25 @@
+/* 
+ * To make copy of the file. We can set trigger and take auto backup of the file at requred time
+ * Output: It will make a copy the file and store to destination folder
+ */
+
+
+function makeCopy() {
+    var folder_id = "XXXXX"; //put destination folder id
+    var file_id = "XXXX"; //put file id
+
+// generates the timestamp and stores in variable formattedDate as year-month-date hour-minute-second
+    var formattedDate = Utilities.formatDate(new Date(), "IST", "yyyy-MM-dd' 'HH:mm:ss");
+
+// gets the name of the original file and appends the word "copy" followed by the timestamp stored in formattedDate
+    var name = SpreadsheetApp.getActiveSpreadsheet().getName() + " Copy " + formattedDate;
+
+// gets the destination folder by their ID. REPLACE xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx with your folder's ID that you can get by opening the folder in Google Drive and checking the URL in the browser's address bar
+    var destination = DriveApp.getFolderById("xxxxxxxxxxxxxxxxxxxxxxxxxxxxxx");
+
+// gets the current Google Sheet file
+    var file = DriveApp.getFileById(SpreadsheetApp.getActiveSpreadsheet().getId())
+
+// makes copy of "file" with "name" at the "destination"
+    file.makeCopy(name, destination);
+}
